0844ab21a7fd42652bb0f6071e083d0552af1750
John Enderson <johnenderson@outlook.com>
2026-06-05 08:02:15 -0300

    fix(error): rename Error component to avoid shadowing built-in

--- app/error.tsx
+++ app/error.tsx
@@ -66,7 +66,7 @@ type ErrorProps = {
   reset: () => void;
 };
 
-export default function Error({ error, reset }: ErrorProps) {
+export default function ErrorPage({ error, reset }: ErrorProps) {
   useEffect(() => {
     console.error(error);
   }, [error]);
